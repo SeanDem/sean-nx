1102c88ded2418b00846f14013c07bcfbdce28ae
Sean Demeritt <sdemeritt3@gatech.edu>
2025-08-15 14:47:04 -0700

    ios and android example

--- apps/example/capacitor.config.ts
+++ apps/example/capacitor.config.ts
@@ -5,11 +5,11 @@ const config: CapacitorConfig = {
   appName: 'test-2',
   webDir: '../../dist/apps/test-2/browser',
   ios: {
-    path: '../../ios/test-2/ios',
+    path: './ios/test-2/ios'
   },
   android: {
-    path: '../../android/test-2/ios',
-  },
+    path: './android/test-2/ios'
+  }
 };
 
 export default config;

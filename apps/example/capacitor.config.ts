4699ff88fa9fd0747460e6576df6a0e66bcabb6c
Sean Demeritt <sdemeritt3@gatech.edu>
2025-08-17 14:34:30 -0700

    example

--- apps/example/capacitor.config.ts
+++ apps/example/capacitor.config.ts
@@ -1,11 +1,13 @@
-import type { CapacitorConfig } from '@capacitor/cli';
+import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'com.sean.nx.example',
+  appId: 'io.ionic.starter',
   appName: 'example',
   webDir: '../../dist/apps/example/browser',
-  ios: { path: './ios' },
-  android: { path: './android' }
+  bundledWebRuntime: false,
+  server: {
+    androidScheme: 'https',
+  },
 };
 
 export default config;
